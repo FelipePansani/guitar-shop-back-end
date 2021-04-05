c09f15e33569ef0f0418e390a81ad4a3e1c680e4
unknown <felipe_pansani@outlook.com>
2021-04-04 22:08:18 -0300

    04042021

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Basses = require('./models/Basses.js')
 const Amps = require('./models/Amps.js')
 const Acoustic = require('./models/Acoustic.js')
 const cors = require('cors');
+require('dotenv');
 
 app.use(cors())
 app.use(express.json())
@@ -13,40 +14,77 @@ app.get('/', (req, res) => {
     res.send('Hello')
 })
 
-app.get('/guitars', (req, res) => {
-    res.send(Guitars)
+app.get('/:category', (req, res) => {
+    let categoryToBeFound = req.params.category;
+
+    switch (categoryToBeFound) {
+        case "guitars":
+            res.send(Guitars)
+            break;
+        case "amps":
+            res.send(Amps)
+            break;
+        case "basses":
+            res.send(Basses)
+            break;
+        case "acoustic":
+            res.send(Acoustic)
+            break;
+        default:
+            res.send("Page not found")
+    }
 })
 
 app.get('/guitars/:brand', (req, res) => {
-    let brandTobeFound = req.params.brand.charAt(0).toUpperCase() + req.params.brand.slice(1)
-    res.send(Guitars.filter(guitar => guitar.brand == brandTobeFound))
-})
+    let brandToBeFound = req.params.brand;
 
-app.get('/basses', (req, res) => {
-    res.send(Basses);
+    if (brandToBeFound == 'all') {
+        res.send(Guitars)
+    }
+    else {
+        res.send(Guitars.filter(guitar => guitar.brand.toLowerCase() == brandToBeFound))
+    }
 })
 
 app.get('/basses/:brand', (req, res) => {
-    let brandTobeFound = req.params.brand.charAt(0).toUpperCase() + req.params.brand.slice(1)
-    res.send(Basses.filter(item => item.brand == brandTobeFound))
-})
+    let brandTobeFound = req.params.brand;
 
-app.get('/amps', (req, res) => {
-    res.send(Amps);
+    if (brandToBeFound == 'all') {
+        res.send(Basses)
+    }
+    else {
+        res.send(Basses.filter(item => item.brand.toLowerCase() == brandTobeFound))
+    }
 })
 
 app.get('/amps/:brand', (req, res) => {
-    let brandTobeFound = req.params.brand.charAt(0).toUpperCase() + req.params.brand.slice(1)
-    res.send(Amps.filter(item => item.brand == brandTobeFound))
-})
-
-app.get('/acoustic', (req, res) => {
-    res.send(Acoustic);
+    let brandTobeFound = req.params.brand;
+    res.send(Amps.filter(item => item.brand.toLowerCase() == brandTobeFound))
 })
 
 app.get('/acoustic/:brand', (req, res) => {
-    let brandTobeFound = req.params.brand.charAt(0).toUpperCase() + req.params.brand.slice(1)
-    res.send(Acoustic.filter(item => item.brand == brandTobeFound))
+    let brandTobeFound = req.params.brand;
+    res.send(Acoustic.filter(item => item.brand.toLowerCase() == brandTobeFound))
 })
 
-app.listen(3000, () => console.log('Express running on port 3000'))
+// >> Entrar em cada produto << //
+app.get('/guitars/item/:product', (req, res) => {
+    res.send(Guitars.filter(item => item.id == req.params.product))
+})
+
+app.get('/basses/item/:product', (req, res) => {
+    res.send(Basses.filter(item => item.id == req.params.product))
+})
+
+app.get('/amps/item/:product', (req, res) => {
+    res.send(Amps.filter(item => item.id == req.params.product))
+})
+
+app.get('/acoustic/item/:product', (req, res) => {
+    res.send(Acoustic.filter(item => item.id == req.params.product))
+})
+// >> Entrar em cada produto << //
+
+const port = 3000;
+
+app.listen(port, () => console.log(`BACKEND is running on port ${port}.`))
